eb19d0db9dfd3f505611005acd7dfc3985680443
Mafalda Rodrigues <mafalda.lmoura@gmail.com>
2023-04-27 18:06:37 +0100

    Add Upcoming Films section

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,11 +24,32 @@ const Home = () => {
       })
   }, [])
 
+  const [upcomingMovies, setUpcomingMovies] = useState([])
+  
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=14c629d9732142aeff7c0bb2d5bbcdcb&language=en-US&page=1')
+      .then(response => response.json())
+      .then(data => {
+
+        const movieCards = data.results.map((obj, i) => {
+          return (
+            <MovieCard movie={obj} key={i} />
+          )
+        })
+        setUpcomingMovies(movieCards)
+      })
+    
+    }, [])
+
 
   return (
     <>
       <Hero text="Welcome to Movie Browser" />
       <div className="container">
+        <h1 className='my-3 fw-5 text-center'>Upcoming Films</h1>
+        <div className="row">
+          {upcomingMovies}
+        </div>
         <h1 className='my-3 fw-5 text-center'>Most Popular Films</h1>
         <div className="row">
           {mostPopular}
